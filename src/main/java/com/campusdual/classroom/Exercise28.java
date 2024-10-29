89d5a0c41bd59bb8ff6647c56548c8d86bd7940e
Adrian Rodriguez Diez <adrian.r.d12389@gmail.com>
2024-10-29 16:36:13 +0100

    Ejercicio 28

--- src/main/java/com/campusdual/classroom/Exercise28.java
+++ src/main/java/com/campusdual/classroom/Exercise28.java
@@ -1,7 +1,11 @@
 package com.campusdual.classroom;
 
+
+
 public class Exercise28 {
     public static void main(String[] args) {
+
+        throw new RuntimeException("Lanzando una excepción desde el método main.");
 
     }
 }
